f7f62561804a8ed2999569056d9827beed398313
Douglas Sironi <ds@mbp-dslocal.local>
2021-07-18 16:00:07 -0300

    add files.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # terraform-libvirt
-Repositorie with example to create a list of machine with terraform file.
+Repositorie with example to create a list of machine with terraform file and configure using init-cloud suporte.
+
+
+#### Requirements
+   - [install-cli](https://learn.hashicorp.com/tutorials/terraform/install-cli)
+   - Install KVM Provider 
+   - Libvirt Enviroment Installed.
+### Get Start
+- ##### Clone and init project.
+
+```sh 
+https://github.com/douglassironi/terraform-libvirt.git
+cd terraform-libvirt
+terraform init 
+```
+- ##### Edit Destination libvirt
+```
+provider "libvirt" {
+  uri   = "qemu+ssh://root@{{YOUR-SERVER}}/system?socket=/var/run/libvirt/libvirt-sock"
+}
+```
+- ##### Define how many machines do you need
+```
+variable "vm_machines" {
+    description = "Create lists of machines."
+    type = list(string)
+    default = ["vm-cnts-7-db","vm-cnts-7-app","vm-cnts-7-impres"]
+}
+```
+<b>  PS: In my case, I created a template with Centos and install a cloud-int package to adjust each machine.
+</b>
+
+#### Run terraform
+```
+terraform apply
+```
+
+After this, the machines will be created and add at local network using bridge.
---
+++ vms-libvirt-creation.tf
@@ -0,0 +1,62 @@
+provider "libvirt" {
+  uri   = "qemu+ssh://root@sr.sironi.local/system?socket=/var/run/libvirt/libvirt-sock"
+}
+
+variable "network" { default = "default" }
+variable "bridge" { default = "br1" }
+
+variable "vm_machines" {
+    description = "Create lists of machines."
+    type = list(string)
+    default = ["vm-cnts-7-db","vm-cnts-7-app","vm-cnts-7-impres"]
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+  vars = {
+    HOSTNAME = var.vm_machines[count.index]
+  }
+
+  count = length(var.vm_machines)
+}  
+
+# Create one init for each machine.
+resource "libvirt_cloudinit_disk" "commoninit" {  
+  name = "commoninit${count.index}.iso"
+  user_data      = data.template_file.user_data[count.index].rendered
+  count = length(var.vm_machines)
+}      
+
+resource "libvirt_volume" "storage" {
+  pool= "storage"  
+  name = "${var.vm_machines[count.index]}.qcow2"
+  count = length(var.vm_machines)
+  base_volume_name = "cts7.template.qcow2"
+}
+
+
+# Machine Creations
+resource "libvirt_domain" "vm" {
+  count = length(var.vm_machines)
+  name = var.vm_machines[count.index]
+  memory = "1024"
+  vcpu = 1
+
+  cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
+
+  network_interface {
+    bridge = "br1"
+    hostname = var.vm_machines[count.index]
+    wait_for_lease = true
+  }
+
+    disk {
+    volume_id = libvirt_volume.storage[count.index].id
+    }
+
+ 
+}
+
+output "ips" {
+  value = libvirt_domain.vm.*.network_interface.0.addresses
+}
